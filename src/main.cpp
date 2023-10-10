0e4c9dcd6628111c533b22b9a7ea795800ba9bab
Joaquin Hernandez <hernandez.joaquin346@gmail.com>
2023-10-10 16:27:17 -0300

    Test de metodos

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,15 @@
 using namespace std;
 int main() {
 	Deuda* deuda1 = new Deuda(1200.0, 6);
-	cout << deuda1->obtenerSaldo();
+	cout <<"\nSaldo:"<< deuda1->obtenerSaldo();
+
+	deuda1->pagar(200.0);
+	cout <<"\nSaldo:"<< deuda1->obtenerSaldo();
+	deuda1->pagar(200.0);
+	deuda1->pagar(300.0);
+	cout <<"\nSaldo:"<< deuda1->obtenerSaldo();
+	cout <<"\nCantidad de pagos:"<< deuda1->contarPagos();
+	cout <<"\nPromedio de pagos:"<< deuda1->calcularPagosPromedio();
 	delete deuda1;
 }
 
