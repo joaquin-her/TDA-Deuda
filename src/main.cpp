c80e59d7c0205470afda5e842b7ece42e9719b23
Joaquin Hernandez <hernandez.joaquin346@gmail.com>
2023-10-10 16:15:39 -0300

    test de compilado con main (bien)

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,14 @@
  *      Author: algo2
  */
 
+#include "Deuda.h"
+#include <iostream>
+using namespace std;
+int main() {
+	Deuda* deuda1 = new Deuda(1200.0, 6);
+	cout << deuda1->obtenerSaldo();
+	delete deuda1;
+}
 
 
 
